a554e4ff5608acf94a582a1d7783924b50f1772a
rajatvarade-globant <rajat.varade@globant.com>
2023-05-23 11:00:13 +0530

    Removing testing dependecies

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -5,7 +5,7 @@ const deployedAppUrl = 'http://172.17.0.3:30000'; // Replace with the URL of you
 describe('Deployed Application Smoke Tests', () => {
   test('GET /hello should return "Hello, World!"', async () => {
     const response = await request(deployedAppUrl).get('/');
-    expect(response.status).toBe(2000);
+    expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
   });
 
