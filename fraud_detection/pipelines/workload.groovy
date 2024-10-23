1613485449c003f667805bc27fa412d465b0f3ee
rajatvarade-globant <rajat.varade@globant.com>
2024-10-24 01:57:04 +0530

    deploy

---
+++ fraud_detection/pipelines/workload.groovy
@@ -0,0 +1,62 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'RELEASE_NAME', defaultValue: 'my-nginx-chart', description: 'Helm release name')
+        string(name: 'CHART_NAME', defaultValue: './fraud_detection/application-chart', description: 'Path or name of Helm chart')
+        string(name: 'NAMESPACE', defaultValue: 'default', description: 'Kubernetes namespace')
+        string(name: 'VALUES_FILE', defaultValue: './fraud_detection/application-chart/values.yaml', description: 'Path to custom values file')
+    }
+
+    stages {
+
+        stage('Connect to the cluster') {
+            steps {
+                sh 'gcloud container clusters get-credentials detection-workloads --zone us-central1-a --project rajatv'
+            }
+        }
+        stage('Helm Version Check') {
+            steps {
+                sh 'helm version'
+            }
+        }
+
+        stage('Deploy Helm Chart') {
+            steps {
+                script {
+                    echo "Deploying Helm chart: ${params.CHART_NAME}"
+                    echo "Using release name: ${params.RELEASE_NAME}"
+                    echo "Namespace: ${params.NAMESPACE}"
+
+                    // Deploy the Helm chart with the provided parameters
+                    sh """
+                    helm upgrade --install ${params.RELEASE_NAME} \
+                                 ${params.CHART_NAME} \
+                                 --namespace ${params.NAMESPACE} \
+                                 -f ${params.VALUES_FILE} \
+                                 --wait
+                    """
+                }
+            }
+        }
+
+        stage('Verify Deployment') {
+            steps {
+                script {
+                    echo "Verifying the deployment in namespace ${params.NAMESPACE}"
+                    // Verify that the pods are running in the desired namespace
+                    sh "kubectl get pods -n ${params.NAMESPACE}"
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Helm deployment succeeded!'
+        }
+        failure {
+            echo 'Helm deployment failed!'
+        }
+    }
+}
