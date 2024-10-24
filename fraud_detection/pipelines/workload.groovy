d3c60bd72f5f73fb4a00ee6da80336688e744494
rajatvarade-globant <rajat.varade@globant.com>
2024-10-24 07:53:52 +0530

    deploy

--- fraud_detection/pipelines/workload.groovy
+++ fraud_detection/pipelines/workload.groovy
@@ -4,6 +4,7 @@ pipeline {
     parameters {
         string(name: 'TAG', , defaultValue: '1.0.0', description: 'please enter the tag in following format 1.0.0')
         string(name: 'RELEASE_NAME', defaultValue: 'my-nginx-chart', description: 'Helm release name')
+        string(name: 'OLD_RELEASE_NAME', defaultValue: 'NEW_DEPLOYMENT', description: 'previous release')
         string(name: 'CHART_NAME', defaultValue: './fraud_detection/application-chart', description: 'Path or name of Helm chart')
         string(name: 'NAMESPACE', defaultValue: 'default', description: 'Kubernetes namespace')
     }
@@ -61,14 +62,61 @@ pipeline {
                 }
             }
         }
+
+        stage(' Test the green deployment') {
+            steps {
+                script {
+                      sh """
+                      curl --silent --show-error --fail 10.4.1.10:80
+
+                    """
+                }
+            }
+        }
+
+        stage(' Making it live  deployment') {
+            steps {
+                script {
+                      sh """
+                      kubectl patch service blue -p '{"spec":{"selector":{"app": "${params.TAG}"}}}'
+                      sleep 5
+
+                    """
+                }
+            }
+        }
+
+         stage(' Test the blue deployment') {
+            steps {
+                script {
+                      sh """
+                      curl --silent --show-error --fail 10.7.247.102:80
+
+                    """
+                }
+            }
+        }
+
+        stage(' Remove the old deployment') {
+             when {
+         expression { params.OLD_RELEASE_NAME != 'NEW_DEPLOYMENT' }
+                 }
+            steps {
+                script {
+                      sh """
+                      helm uninstall ${params.OLD_RELEASE_NAME}
+                    """
+                }
+            }
+        }
     }
 
     post {
         success {
-            echo 'Helm deployment succeeded!'
+            echo 'deployment succeeded!'
         }
         failure {
-            echo 'Helm deployment failed!'
+            echo 'deployment failed!'
         }
     }
 }
